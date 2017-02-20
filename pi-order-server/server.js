c95242f73c1deb163943b1f77c6fc04500bd6468
Joe Dye <jdye94@gmail.com>
2017-02-20 13:54:44 -0500

    another folder change

--- pi-order-server/server.js
+++ pi-order-server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use('/styles', express.static(path.join(__dirname, 'styles')));
+app.use('/styles', express.static(__dirname + '/styles'));
 app.set('view engine', 'pug');
 
 var port = 9090; // set our port
