ff357ad4d36813b243d27790bd2746b79f9bcf8a
Joe Dye <jdye94@gmail.com>
2017-02-20 13:40:59 -0500

    testing folder change

--- pi-order-server/server.js
+++ pi-order-server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use("/styles", express.static(__dirname + "/pi-order/styles"));
+app.use("/styles", express.static("/styles"));
 app.set('view engine', 'pug');
 
 var port = 9090; // set our port
